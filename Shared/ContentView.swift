a11b33ad47109ae24b0739faf59596df25bbab65
GerardoGarzon <ggonzalezg1300@alumno.ipn.mx>
2022-06-29 19:32:02 -0500

    Card view

--- Shared/ContentView.swift
+++ Shared/ContentView.swift
@@ -9,13 +9,36 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello there, ESCOM!")
-            .padding()
+        HStack {
+            CardView(isFaceUp: true)
+            CardView(isFaceUp: false)
+            CardView(isFaceUp: true)
+            CardView(isFaceUp: false)
+        }
+        .padding(.horizontal)
+    }
+}
+
+struct CardView: View {
+    var isFaceUp: Bool
+    
+    var body: some View {
+        ZStack{
+            let shapeCard = RoundedRectangle(cornerRadius: 25.0)
+            if isFaceUp {
+                shapeCard.fill().foregroundColor(Color.white)
+                shapeCard.stroke(lineWidth: 3.0).foregroundColor(Color.orange)
+                Text("🚀").font(.largeTitle)
+            } else {
+                shapeCard.fill().foregroundColor(Color.orange)
+            }
+        }
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+            .preferredColorScheme(.light)
     }
 }
